3206f15d8f0d57e848bcda2e6f4d97b29d9fe3f2
Jasmine Boparai <jasmine.boparai@vsp.com>
2017-03-20 15:33:08 -0700

    revised code after review.

--- app/src/main/java/com/example/android/myquiz/MainActivity.java
+++ app/src/main/java/com/example/android/myquiz/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.android.myquiz;
 
+// importing packages
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -10,10 +11,12 @@ import android.widget.RadioButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import static com.example.android.myquiz.R.id.answer1;
 
+public class MainActivity extends AppCompatActivity {
     int score = 0;
     int finalScore = 0;
+
 
 
     @Override
@@ -41,49 +44,53 @@ public class MainActivity extends AppCompatActivity {
         boolean checkedAlan = false;
         boolean checkedJasmine = false;
 
-        EditText answerOne = (EditText) findViewById(R.id.answer1);
+        EditText answerOne = (EditText) findViewById(answer1);
         String answer1 = answerOne.getText().toString().trim();
-        if (answer1.matches("Blue")) {
-            score++;
-        }
-        CheckBox melvillCheckBox = (CheckBox) findViewById(R.id.Melvill);
-        checkedMelvill = melvillCheckBox.isChecked();
-
-        CheckBox alanCheckBox = (CheckBox) findViewById(R.id.Alan);
-        checkedAlan = alanCheckBox.isChecked();
-
-        CheckBox jasmineCheckBox = (CheckBox) findViewById(R.id.Jasmine);
-        checkedJasmine = jasmineCheckBox.isChecked();
-
-        EditText answerThree = (EditText) findViewById(R.id.answer3);
-        String answer3 = answerThree.getText().toString().trim();
-
-        if (answer3.matches("Blue,Red,Green")) {
+        if (answer1.equalsIgnoreCase("Blue")) {
             score++;
         }
 
-        CheckBox mercuryCheckBox = (CheckBox) findViewById(R.id.Mercury);
-        checkedMercury = mercuryCheckBox.isChecked();
 
-        CheckBox plutoCheckBox = (CheckBox) findViewById(R.id.Pluto);
-        checkedPluto = plutoCheckBox.isChecked();
+            CheckBox melvillCheckBox = (CheckBox) findViewById(R.id.Melvill);
+            checkedMelvill = melvillCheckBox.isChecked();
 
-        RadioButton radiobutton1 = (RadioButton) findViewById(R.id.RadioButton1);
-        checkedRadioButton1 = radiobutton1.isChecked();
+            CheckBox alanCheckBox = (CheckBox) findViewById(R.id.Alan);
+            checkedAlan = alanCheckBox.isChecked();
 
-        RadioButton radioButton2 = (RadioButton) findViewById(R.id.RadioButton2);
-        checkedRadioButton2 = radioButton2.isChecked();
+            CheckBox jasmineCheckBox = (CheckBox) findViewById(R.id.Jasmine);
+            checkedJasmine = jasmineCheckBox.isChecked();
 
-        finalScore = calculateScore(checkedJasmine, checkedAlan, checkedMelvill, checkedMercury, checkedPluto, checkedRadioButton1, checkedRadioButton2);
+            EditText answerThree = (EditText) findViewById(R.id.answer3);
+            String answer3 = answerThree.getText().toString().trim();
 
-        displayMessage("Your Score is:" + finalScore);
-    }
+            if (answer3.equalsIgnoreCase("Obama"))
+            {
+                score++;
+            }
+
+            CheckBox mercuryCheckBox = (CheckBox) findViewById(R.id.Mercury);
+            checkedMercury = mercuryCheckBox.isChecked();
+
+            CheckBox plutoCheckBox = (CheckBox) findViewById(R.id.Pluto);
+            checkedPluto = plutoCheckBox.isChecked();
+
+            RadioButton radiobutton1 = (RadioButton) findViewById(R.id.RadioButton1);
+            checkedRadioButton1 = radiobutton1.isChecked();
+
+            RadioButton radioButton2 = (RadioButton) findViewById(R.id.RadioButton2);
+            checkedRadioButton2 = radioButton2.isChecked();
+
+            finalScore = calculateScore(checkedJasmine, checkedAlan, checkedMelvill, checkedMercury, checkedPluto, checkedRadioButton1, checkedRadioButton2);
+
+            displayMessage("Your Score is:" + finalScore);
+        }
+
+
 
     //calculate score
     private int calculateScore(boolean checkedJasmine, boolean checkedAlan, boolean checkedMelvill, boolean checkedMercury, boolean checkedPluto, boolean checkedRadioButton1, boolean checkedRadioButton2) {
 
-
-        if (checkedAlan) {
+        if ((checkedAlan && checkedMelvill) && !(checkedJasmine)) {
             score++;
         }
 
@@ -95,8 +102,11 @@ public class MainActivity extends AppCompatActivity {
         if (checkedRadioButton2) {
             score++;
         }
+
+
         return score;
     }
+
 
     //Print Score
     private void displayMessage(String message) {
@@ -106,7 +116,7 @@ public class MainActivity extends AppCompatActivity {
         if (finalScore == 5) {
             Toast.makeText(this, "Congratulations! You have a perfect score!", Toast.LENGTH_SHORT).show();
         }
-        if (finalScore == 4) {
+        else if (finalScore == 4) {
             Toast.makeText(this, "Your final score is 4!", Toast.LENGTH_SHORT).show();
         } else if (finalScore == 3) {
             Toast.makeText(this, "Your final score is 3!", Toast.LENGTH_SHORT).show();
@@ -123,7 +133,7 @@ public class MainActivity extends AppCompatActivity {
     // Reset Score
     public void Reset(View v) {
         score = 0;
-        //finalScore = 0;
+        finalScore = 0;
         displayMessage("Score set to Zero");
         RadioButton radioButton1 = (RadioButton) findViewById(R.id.RadioButton1);
         RadioButton radioButton2 = (RadioButton) findViewById(R.id.RadioButton2);
@@ -139,7 +149,7 @@ public class MainActivity extends AppCompatActivity {
         jasmineCheckBox.setChecked(false);
         mercuryCheckBox.setChecked(false);
         plutoCheckBox.setChecked(false);
-        EditText answerOne = (EditText) findViewById(R.id.answer1);
+        EditText answerOne = (EditText) findViewById(answer1);
         answerOne.setText("");
         EditText answerThree = (EditText) findViewById(R.id.answer3);
         answerThree.setText("");
